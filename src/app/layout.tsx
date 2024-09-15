d73605d8da2d21cc30bb7bc4f2b955242c86aa12
Kratospidey <devtools@kratospidey.com>
2024-09-16 02:15:16 +0530

    fix linting error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-	src: "./fonts/GeistVF.woff",
-	variable: "--font-geist-sans",
-	weight: "100 900",
-});
-const geistMono = localFont({
-	src: "./fonts/GeistMonoVF.woff",
-	variable: "--font-geist-mono",
-	weight: "100 900",
-});
+
 
 import { Inter } from "next/font/google";
 
@@ -33,7 +24,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${geistSans.variable} ${inter.variable} antialiased`}>
+			<body className={`${inter.variable} antialiased`}>
 				{children}
 			</body>
 		</html>
